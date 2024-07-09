8b246903000758c8ab1520dd97f8bab999bc0cec
LiamSnow <mail@liamsnow.com>
2024-07-09 13:01:20 -0400

    updated schema

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -9,21 +9,18 @@ const schema = a.schema({
       start: a.datetime(),
       end: a.datetime(),
       payment: a.float(),
-      location: a.ref("Location"),
+      lat: a.float(),
+      long: a.float(),
+      radius: a.float()
     })
     .authorization((allow) => [allow.publicApiKey()]),
-
-  Location: a.customType({
-    lat: a.float(),
-    long: a.float(),
-  }),
 
   Athlete: a
     .model({
       id: a.id(), //FIXME Is this right for cognito id ref?
       attended_events: a.integer(),
       missed_events: a.integer(),
-      balance: a.float(),
+      deposited: a.float(), //balance = sum attended_events - deposited
     })
     .authorization((allow) => [allow.publicApiKey()]),
 });
