c10d25c2097987913b919688f2920ab7396958f7
JohnnyWobble <tonald.drump2.0@gmail.com>
2024-07-10 15:48:24 -0400

    Updated database schema to include transactions and one-to-many relationships

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,25 +3,32 @@ import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 const schema = a.schema({
   Event: a
     .model({
-      id: a.id(),
       name: a.string().required(),
-      type: a.string().required(),
+      type: a.enum(["Practice", "Class", "Tutoring"]),
       start: a.datetime().required(),
       end: a.datetime().required(),
       payment: a.float().required(),
       lat: a.float().required(),
       long: a.float().required(),
       radius: a.float().default(5),
+      attended: a.boolean().required().default(false),
+      athleteId: a.id().required()
     })
     .authorization((allow) => [allow.publicApiKey()]),
 
   Athlete: a
     .model({
-      id: a.string(),
-      attended_events: a.id().array(),
-      deposited: a.float().default(0), //balance = sum attended_events - deposited
+      events: a.hasMany("Event", "athleteId"),
+      transactions: a.hasMany("Transaction", "athleteId"),
+      // deposited: a.float().default(0), //balance = sum attended_events - deposited
     })
     .authorization((allow) => [allow.publicApiKey()]),
+
+  Transaction: a.model({
+      athleteId: a.id(),
+      amount: a.float().required(),
+      isBankDeposit: a.boolean().required(),
+  }).authorization((allow) => [allow.publicApiKey()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
